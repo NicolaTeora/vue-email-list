24304d8fdbdc79ada4904066a8632b76d7230587
NicolaTeora <nikxodia@gmail.com>
2024-02-06 17:12:51 +0100

    feat : print in DOM email random

--- JS/main.js
+++ JS/main.js
@@ -4,19 +4,24 @@ const app = createApp({
     data() {
         return {
             title: 'Hello world',
+            listEmail: [],
         };
     },
 
     methods: {},
 
-    mounted() {},
+    created(){},
+
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response)=>{        
+                const userEmail = response.data.response
+                this.listEmail.push(userEmail)
+                console.log(userEmail);
+            })
+        }
+    },
 });
 
 app.mount('#app')
 
-for (let i = 0; i < 10; i++) {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response)=>{        
-        const userEmail = response.data.response
-        console.log(userEmail);
-    })
-}
